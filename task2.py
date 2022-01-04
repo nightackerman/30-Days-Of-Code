02301f51f41dff68042a2cfaa8212c23cb1b5299
nightackerman <prajapatidivya1369@gmail.com>
2022-01-04 14:48:04 +0530

    day1

---
+++ task2.py
@@ -0,0 +1,3 @@
+str1 = input("enter first string: ")
+str2  = input("enter second string: ")
+print(str1+str2)
---
+++ task3.py
@@ -0,0 +1,4 @@
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+add = num1+num2
+print("Addition: ",add )
