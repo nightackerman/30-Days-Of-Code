7d52de11d3fc877b01e0d86f605045c40be86019
nightackerman <prajapatidivya1369@gmail.com>
2022-01-06 18:03:21 +0530

    day3

---
+++ DAY3/task1.py
@@ -0,0 +1,7 @@
+x = float(input("First number: ")) 
+y = float(input ( "Second number: "))  
+      
+res = x % y 
+print("Modulus of two float number is: ", x, "%", y, " = ", res, )
+print("power of two number is: ", pow(x,y))
+print("Floor of two number is: ", x//y)
